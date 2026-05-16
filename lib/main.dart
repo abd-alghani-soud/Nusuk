cfa2a74e31e88b29311f7e949946357b0c153697
Bahaa <bahaamarstawe@gmail.com>
2026-05-16 11:50:33 -0700

    create splash page body and add images folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nusuk/features/home/presentation/page/home_page.dart';
+import 'package:nusuk/features/splash/presentation/page/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       },
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()),
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage()),
     );
   }
 }
